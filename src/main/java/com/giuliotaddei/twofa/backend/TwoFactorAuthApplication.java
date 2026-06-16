58aa64ca829c20ecb8a2b27605df0f0bcd06bcb0
giuliotaddei <giuliotaddei1998@gmail.com>
2026-06-16 17:29:19 +0200

    first commit

---
+++ src/main/java/com/giuliotaddei/twofa/backend/TwoFactorAuthApplication.java
@@ -0,0 +1,13 @@
+package com.giuliotaddei.twofa.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwoFactorAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwoFactorAuthApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/giuliotaddei/twofa/backend/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.giuliotaddei.twofa.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "Backend OK - Spring Boot is running";
+    }
+}
